6da6b8879a7dff770639cf548595f219094f7bd8
msb <mike@msb.me.uk>
2018-09-27 15:10:38 +0100

    bugfix: make sure that arguments are passed to tox

--- tox.sh
+++ tox.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 #
-# Wrapper script to run test-suite within docker image. Arguments to the script
-# are passed to tox.
+# Wrapper script to run tox. Arguments are passed directly to tox.
+
+# Exit on failure
+set -e
 
 # Change to this script's directory
 cd "$( dirname "${BASH_SOURCE[0]}")"
 
-# Run test suite
-exec ./compose.sh tox run --rm tox $@
+# Execute tox runner, logging command used
+set -x
+exec ./compose.sh tox run --rm $COMPOSE_ARGS tox tox $@
